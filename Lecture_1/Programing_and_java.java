fd29a1c4c198df7c5daac15a0d63f34124130441
44502699 <seong-seok.lee@students.mq.edu.au>
2020-12-04 20:43:22 +0900

    Delete dong & Add int variable

--- Lecture_1/Programing_and_java.java
+++ Lecture_1/Programing_and_java.java
@@ -3,6 +3,9 @@ package Lecture_1;
 public class Programing_and_java {
 
     public static void main(String[] arg){
-        System.out.println("Hi"); 
+        int a = 5; 
+        int b = 3;
+        System.out.println("Hi This is the first App"); 
+        System.out.println(a + b);   
     }
 }
